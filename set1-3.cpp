5855f34f1fd81988d1643546a60c551811175772
Srinivasan407 <36328035+Srinivasan407@users.noreply.github.com>
2018-03-20 19:31:59 +0530

    set1-3.cpp

---
+++ set1-3.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	char c;
+	cout<<"Enter a character: ";
+	cin>>c;
+	if((c>='a'&& ch<='z') || (c>='A' && c<='Z'))
+	{
+		cout<<" Alphabet";
+	}
+	else
+	{
+		cout<<" No Alphabet";
+	}
+	getch();
+}
