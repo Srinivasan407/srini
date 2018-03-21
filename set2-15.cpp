9eb469cb54bcc5b7ce2b90ad9249e212b7d41aa9
Srinivasan407 <36328035+Srinivasan407@users.noreply.github.com>
2018-03-21 20:13:14 +0530

    set2-15.cpp

---
+++ set2-15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int m,n;
+	cin>>m;
+	cin>>n;
+	for(int i=m+1;i<n;i++)
+	{
+	    if(i%2==0)
+	    {
+	        cout<<i<<endl;
+	    }
+	}
+	return 0;
+}
